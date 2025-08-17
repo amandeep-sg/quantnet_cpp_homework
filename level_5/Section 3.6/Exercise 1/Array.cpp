aec17f146d75b8e7fd22c410e9a4c43e1b8279d7
amandeep.sg <amandeepsgujral93@gmail.com>
2025-08-17 12:47:23 +0530

    level 5 section 3.6 exercise 1 completed

---
+++ level_5/Section 3.6/Exercise 1/Array.cpp
@@ -0,0 +1,114 @@
+// Array.cpp
+// program to define the Array class i.e. constructors, member functions
+//
+// author: amandeep singh gujral
+
+#include <iostream>
+
+#include "Array.hpp"
+#include "Point.hpp"
+
+using namespace std;
+
+namespace ADSINGH
+{
+    namespace CONTAINER
+    {
+        Array::Array() : size(5) // default constructor initialise array of size 5
+        {
+            arr = new CAD::Point[size];
+            cout << "default constructor called: array initialise of size 5" << endl;
+        }
+
+        Array::Array(int size) : size(size) // constructor initialises the array of size defines by client
+        {
+            arr = new CAD::Point[size];
+            cout << "constructor called: array initialised of size " << size << endl;
+        }
+
+        Array::Array(Array &source) // copy constructor
+        {
+            size = source.size;
+            arr = new CAD::Point[size];
+            if (arr == nullptr) // check if the memory is assigned in the heap
+            {
+                cout << "error: insufficient memory!" << endl;
+            }
+            else
+            {
+                for (int i = 0; i < size; i++) // for loop to copy each element of the source array to this.
+                {
+                    arr[i] = source.arr[i];
+                }
+            }
+            cout << "constructor called: array of size " << size << " copied!" << endl;
+        }
+
+        Array::~Array() // destructor to delete the array when out of scope
+        {
+            delete[] arr;
+            // cout << "deleted array of size " << size << endl;
+        }
+
+        Array &Array::operator=(Array &source) // assigment operator to copy element of one array to another
+        {
+            if (this == &source) // check if the source array == *this to avoid deletion of the elements
+            {
+                return *this;
+            }
+            else
+            {
+                delete[] arr;       // delete the element of the this array;
+                size = source.size; // set the size of this array equal to source;
+                arr = new CAD::Point[size];
+
+                if (arr == nullptr) // check if the memory is assigned in the heap
+                {
+                    cout << "error: insufficient memory!" << endl;
+                }
+                else
+                {
+                    for (int i = 0; i < size; i++) // for loop to copy each element of the source array to this.
+                    {
+                        arr[i] = source.arr[i];
+                    }
+                }
+            }
+            return *this;
+        }
+
+        int Array::Size() // get the size of the array instance
+        {
+            return size;
+        }
+
+        CAD::Point &Array::GetElement(int index) const // get the element of the array instance based on index argument
+        {
+            if (index < 0 || index >= size)
+            {
+                throw -1; // throw -1 if index is out of range
+            }
+            return arr[index];
+        }
+
+        void Array::SetElement(const int index, const CAD::Point &element) // set the Point element at the index in an array
+        {
+            if (index < 0 || index >= size)
+            {
+                throw -1; // through -1 if index is out of range
+            }
+            arr[index] = element;
+        }
+
+        const CAD::Point &Array::operator[](int index) const // overload operator[] to get element of an array instance created with const
+        {
+            return GetElement(index);
+        }
+
+        CAD::Point &Array::operator[](int index) // overload operator[] to get element of an array instance created without const
+        {
+
+            return GetElement(index);
+        }
+    }
+}
