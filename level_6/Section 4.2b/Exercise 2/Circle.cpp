84ca07c7b0ffdea651500f33c48afc46b094f37f
amandeep.sg <amandeepsgujral93@gmail.com>
2025-08-23 14:39:09 +0530

    section 4.2b exercise 2 initiated

---
+++ level_6/Section 4.2b/Exercise 2/Circle.cpp
@@ -0,0 +1,98 @@
+// Circle.cpp
+// program to define the behaviour of constructor, destructor and member functions of class Circle
+//
+// author: amandeep singh gujral
+
+#include <iostream>
+#include <sstream>
+#include <ostream>
+
+#include "Point.hpp"
+#include "Circle.hpp"
+
+namespace ADSINGH
+{
+    namespace CAD
+    {
+        Circle::Circle() : Shape(), center_point(Point()), raidus(0.00) // initialise Circle with raidus 0.00 and center point (0.00, 0.00)
+        {
+            // cout << "Constructor called: New Circle created!";
+        };
+
+        Circle::Circle(const Point &c, double r) : Shape(), center_point(c), raidus(r) // overload the constructor to initialize Circle with point a & b
+        {
+            cout << "Constructor called: New Circle created with center  " << c.ToString() << " and raidus " << r << endl;
+        };
+
+        Circle::Circle(const Circle &other) : Shape(other), center_point(other.center_point), raidus(other.raidus) // copy constructor
+        {
+            // cout << "Constructor called: " << other.ToString() << " copied!" << endl;
+        };
+
+        Point Circle::Center_Point() const // get start point of the Circle
+        {
+            return center_point;
+        }
+
+        Circle &Circle::operator=(const Circle &source) // assignment operator
+        {
+            Shape::operator=(source);
+            center_point = source.center_point;
+            raidus = source.raidus;
+            return *this;
+        }
+
+        double Circle::Raidus() const // get end point of the Circle
+        {
+            return raidus;
+        }
+
+        double Circle::Diameter() const // get the diameter of the circle
+        {
+            return 2 * raidus;
+        }
+
+        double Circle::Area() const // get the area of the circle
+        {
+            return M_PI * pow(raidus, 2);
+        }
+
+        double Circle::Circumference() const // get the circumference of the circle
+        {
+            return 2 * M_PI * raidus;
+        }
+
+        void const Circle::Draw() // draw circle in the cartesian plane
+        {
+            cout << "draw circle " << *this << endl;
+        }
+
+        void Circle::Center_Point(Point &p) // update start point of the Circle
+        {
+            this->center_point = p;
+        }
+
+        void Circle::Raidus(double &r) // update end point of the Circle
+        {
+            this->raidus = r;
+        }
+
+        string Circle::ToString() const // return desctiption of the Circle in string
+        {
+            stringstream ss;
+            ss << "Circle Center " << center_point << " | Circle Raidus " << raidus << " | Circle " << Shape::ToString();
+            return ss.str();
+        }
+
+        ostream &operator<<(ostream &os, const Circle &source) //<< operator overload to cout
+        {
+            os << "circle has center " << source.center_point << "and raidus " << source.raidus;
+            return os;
+        }
+
+        Circle::~Circle()
+        {
+            // cout << ToString() << " is destroyed" << endl;
+        }
+    }
+}
---
+++ level_6/Section 4.2b/Exercise 2/Main.cpp
@@ -0,0 +1,48 @@
+// Main.cpp
+// Initialise an array of type Array of size 3
+// Assign Point to each element of array
+// get and set the elements of the array
+//
+// author: amandeep singh gujral
+
+#include <iostream>
+
+#include "Shape.hpp"
+#include "Array.hpp"
+#include "Point.hpp"
+#include "Line.hpp"
+#include "Circle.hpp"
+#include "OutOfBoundsException.hpp"
+#include "ArrayException.hpp"
+
+using namespace std;
+using namespace ADSINGH::CAD;
+using namespace ADSINGH::CONTAINER;
+
+int main()
+{
+     Array<int> intArray1;
+     Array<int> intArray2;
+     Array<double> doubleArray;
+     cout << intArray1.DefaultSize() << endl;
+     cout << intArray2.DefaultSize() << endl;
+     cout << doubleArray.DefaultSize() << endl;
+     intArray1.DefaultSize(15);
+     cout << intArray1.DefaultSize() << endl;
+     cout << intArray2.DefaultSize() << endl;
+     cout << doubleArray.DefaultSize() << endl;
+     return 0;
+}
+
+// following values are printed [Note: the default value (is hardcoded) = 5]
+// 5
+// 5
+// 5
+// 15
+// 15
+// 5
+// Explanation:
+// Static members are shared across all the instances of the class
+// However, in case of templates, there are three instances 2 of class with type int and 1 of class with type double
+// the static member of class with type int is updated in the program
+// hence no impact on the instance of class with type double
