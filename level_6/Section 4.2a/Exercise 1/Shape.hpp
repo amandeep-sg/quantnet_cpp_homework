b8cd42714a08f00729d02099775ffc50bc71d279
amandeep.sg <amandeepsgujral93@gmail.com>
2025-08-23 11:57:30 +0530

    section 2.4a exercise 1 initiated

---
+++ level_6/Section 4.2a/Exercise 1/Shape.hpp
@@ -0,0 +1,39 @@
+// Shape.hpp
+// program to declare the Shape base class constructor, copy constructor, destructor
+// operator= overload, getter and virtual ToString member functions
+// declare Draw and Print pure virtual member functions
+//
+// author: amandeep singh gujral
+
+#ifndef Shape_hpp
+#define Shape_hpp
+
+#include <string>
+
+namespace ADSINGH
+{
+    namespace CAD
+    {
+        class Shape
+        {
+        private:
+            int id;
+
+        public:
+            Shape();                    // default constructor
+            Shape(const Shape &source); // copy constructor
+            virtual ~Shape();           // destructor
+
+            Shape &operator=(const Shape &source); // operator= overload
+
+            const int ID() const;                 // return the id of the shape instance
+            virtual std::string ToString() const; // ToString to return string output ex. "ID: 123"
+
+            // pure virtual member functions
+            virtual void Draw() = 0;  // draw the shape in the cartesian plane
+            virtual void Print() = 0; // print shape information
+        };
+    }
+}
+
+#endif
